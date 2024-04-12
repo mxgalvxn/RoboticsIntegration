566a2a6763030b4835082aab7fa05dd6bbd424da
Monserrat Galván <55207425+mxgalvxn@users.noreply.github.com>
2024-04-11 21:01:11 -0600

    Update roverSinlinearizar.py

--- Rove/roverSinlinearizar.py
+++ Rove/roverSinlinearizar.py
@@ -1,6 +1,5 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import math 
 
 wheelbase = 0.5
 dt = 0.1
@@ -9,30 +8,33 @@ w_max = np.pi / 4
 
 def control(t):
     v = v_max * np.sin(0.5 * t)
-    w = 0.5 * np.cos(t)
+    w = 0.8 * np.cos(t)
     return v, w
 
 def simulate():
     x = 0.0
     y = 0.0
     theta = 0.0
-    theta_pos = []
+    theta_pos = [0]
+    time_values = [0]
     path = [(x, y)]
 
     for t in np.arange(0, 5, dt):
+
         v, w = control(t)
-        #v_left = v - w * wheelbase / 2
-        v_left = .4
-        #v_right = v + w * wheelbase / 2
-        v_right = .5
+        v_left = v - w * wheelbase / 2
+        #v_left = .4
+        v_right = v + w * wheelbase / 2
+        #v_right = .5
         x += (v_left + v_right) / 2 * np.cos(theta) * dt
         y += (v_left + v_right) / 2 * np.sin(theta) * dt
         theta += (v_right - v_left) / wheelbase * dt
-        theta_pos.append(theta)
         print(theta)
         path.append((x, y))
         plt.clf()
         plot_path(path)
+        #plt.plot(theta)
+        plot_orientation(theta)
         plt.pause(dt)
 
 def plot_path(path):
@@ -44,6 +46,13 @@ def plot_path(path):
     plt.title('carrito')
     plt.grid(True)
     plt.axis('equal')
+
+def plot_orientation(theta_values):
+    plt.plot(theta_values, '-')
+    plt.xlabel('Tiempo (segundos)')
+    plt.ylabel('Theta (radianes)')
+    plt.title('Orientación del Carrito Pioneer')
+    plt.grid(True)
 
 simulate()
 plt.show()
